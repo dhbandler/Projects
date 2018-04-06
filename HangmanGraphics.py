b2daf1b53f122a7beb2b2ce0bb260bb102eedd43
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-04-06 08:54:40 -0400

    Updated from Brython Server: 4/6/2018 8:54:40 AM

--- HangmanGraphics.py
+++ HangmanGraphics.py
@@ -7,25 +7,35 @@ from random import randint
 def pickWord():  #Selects the word
     num = randint(1,10)
     if num == 1:
-        word = "Happiness"
+        word = "happiness"
+        return word
     elif num == 2:
-        word = "Mechanized"
+        word = "mechanized"
+        return word
     elif num == 3:
-        word = "Wedding"
+        word = "wedding"
+        return word
     elif num == 4:
-        word = "Airport"
+        word = "airport"
+        return word
     elif num == 5:
-        word = "Realpolitik"
+        word = "realpolitik"
+        return word
     elif num == 6:
-        word = "Beelzebub"
+        word = "beelzebub"
+        return word
     elif num == 7:
-        word = "Conquest"
+        word = "conquest"
+        return word
     elif num == 8:
-        word = "Schadenfreude"
+        word = "schadenfreude"
+        return word
     elif num == 9:
-        word = "Intelligentsia"
+        word = "intelligentsia"
+        return word
     else:
-        word = "Carthage"
+        word = "carthage"
+        return word
         
 def charact(): #Prints lines under letters for guess
     charcount = len(word)
@@ -34,14 +44,36 @@ def charact(): #Prints lines under letters for guess
         Sprite(wordunderline, (z, 100))
         z += 75
         
+      
+def keyPress(abcdefghijklmnopqrstuvwxyz):
+    if i in word:
+        text = TextAsset(i, fill=black,style= "bold 30pt Georgia")
+        Sprite(text)
+        data["correct guesses"] += 1
+    else:
+        text = TextAsset(i, fill=red,style= "bold 30pt Georgia")
+        Sprite(text)
+        data["incorrect guesses"] += 1 
+        if data["incorrect guesses"] == 1:
+            Sprite(head, (200,140))
+        elif data["incorrect guesses"] == 2:
+            Sprite(body, (235,220))
+        elif data["incorrect guesses"] == 3:
+            Sprite(limbs, (225,295))
+        elif data["incorrect guesses"] == 4:
+            Sprite(limbs, (245,295)) 
+        elif data["incorrect guesses"] == 5:
+            Sprite(blackLine, (228, 160))
+        elif data["incorrect guesses"] == 6:
+            Sprite(blackLine2, (233, 225))
+            Sprite((TextAsset("you died", fill=red,style= "bold 40pt Georgia")), 200, 200)
     
+   
 
 if __name__ == '__main__':
-
     data = {}
     data["incorrect guesses"] = 0
-    data["total guesses"] = 0
-    
+    data["correct guesses"] = 0
 
 
     #Colors:
@@ -62,9 +94,12 @@ if __name__ == '__main__':
     rope2 = EllipseAsset(15,30,blackOutline2,lightbrown)
     ropespace = EllipseAsset(12,27,blackOutline2,white)
     head = CircleAsset(40,blackOutline,white)
-    body = RectangleAsset(50,75, blackOutline, white)
-    limbs = RectangleAsset(25,50, blackOutline, white)
+    body = RectangleAsset(5,75, blackOutline, white)
+    limbs = RectangleAsset(5,50, blackOutline, white)
+    blackLine = LineAsset(0,0, blackOutline)
+    blackLine2 = LineAsset(50,50, blackOutline)
     wordunderline = RectangleAsset(15,5, blackOutline, black)
+    
 
     #Sprites constants
     Sprite(wood1, (50,50))
@@ -73,8 +108,12 @@ if __name__ == '__main__':
     Sprite(rope1, (240,75))
     Sprite(rope2, (228,150))
     Sprite(ropespace, (231, 153))
-
-    for i in range():
+    
+    
+    
+    for i in str("abcdefghijklmnopqrstuvwxyz"):
+        App().listenKeyEvent("keydown", i, keyPress)
         
-
+        
+    
     App().run(pickWord)
