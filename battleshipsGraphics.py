9ea97390e2025ca3cc5bc4909432995be6e7097a
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-05-23 09:29:49 -0400

    Updated from Brython Server: 5/23/2018 9:29:47 AM

--- battleshipsGraphics.py
+++ battleshipsGraphics.py
@@ -1,1 +1,29 @@
+#Daniel Bandler
+#5/23/18
+#battleshipsGame.py
 
+from ggame import *
+
+ROWS = 16
+COLS = 34
+CELL_SIZE = 30
+
+buildBoard = []
+
+
+if __name__== "__main__":
+    
+    blue = Color(0x3383FF,1)
+    chrome = Color(0xdbe4eb,1)
+    black = Color(0x000000,1)
+    
+    seaBox = RectangleAsset(CELL_SIZE*COLS,CELL_SIZE*ROWS,LineStyle(1,blue),blue)
+    shipBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,black),chrome)
+    line = LineAsset(1*COLS,CELL_SIZE*ROWS, LineStyle(1,black))
+    
+
+    Sprite(seaBox)
+    Sprite(line)
+    Sprite(shipBox)
+    App().run()
+    
