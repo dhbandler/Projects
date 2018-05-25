20f80c35b23684dddc8a462b1c4e0c2e2a70657e
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-05-25 08:50:05 -0400

    Update battleshipsGraphics.py

--- battleshipsGraphics.py
+++ battleshipsGraphics.py
@@ -11,8 +11,9 @@ CELL_SIZE = 100
 
 def buildBoard():
     board = [['a','b','c','d','e'],['f','g','h','i','j'],['k','l','m','n','o'],['p','q','r','s','t'],['u','v','w','x','y']]
+    #board2 = [['a','b','c','d','e'],['f','g','h','i','j'],['k','l','m','n','o'],['p','q','r','s','t'],['u','v','w','x','y']]
     return board 
-    
+
 def redrawAll():
     for item in App().spritelist[:]:
         item.destroy()
@@ -21,13 +22,14 @@ def redrawAll():
         for j in range(5):
             Sprite(RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(3,black),blue),(i*CELL_SIZE, j*CELL_SIZE))
 
-
+            
 
 def mouseClick(event):
     totalClicks == 1
     if totalClicks < 3:
         Sprite(shipbox, (mouseClick.x,mouseClick.y))
     
+"""
 def pickComputerShips():
     i = 0
     while i <= 3:
@@ -35,9 +37,9 @@ def pickComputerShips():
         rand2 = randint(1,5)
         Sprite(shipbox,(board[rand1-1][rand2-1]))
         i += 1
-    
+"""    
 
-def computerTurn():
+#def computerTurn():
 
     
 
@@ -51,12 +53,12 @@ if __name__== "__main__":
     black = Color(0x000000,1)
 
 
-    shipBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,black),chrome)
+    shipbox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(3,black),chrome)
 
-
+    buildBoard()
     redrawAll()
     
-    pickComputerShips()
+    #pickComputerShips()
 
     #App.listenMouseEvent("click", mouseClick)
 
