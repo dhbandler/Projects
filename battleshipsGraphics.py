a0b5de7087ddcec8964eeb40c902c269caad9e46
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-06-07 08:27:27 -0400

    Updated from Brython Server: 6/7/2018 8:27:26 AM

--- battleshipsGraphics.py
+++ battleshipsGraphics.py
@@ -5,7 +5,7 @@
 from ggame import * #imports everything from ggame
 from random import randint #imports the tools for generating a random integer
 
-rowcols = int(input("Enter the number of rows and columns you want.  ")) #asks for number of rows and columns
+rowcols = int(input("Enter the number of rows and columns you want. (max of 5 please)  ")) #asks for number of rows and columns
 shipNum = int(input("Enter the number of subs you want.  ")) #asks for number of ships
 
 
@@ -44,47 +44,50 @@ def redrawAll(): #handles all of the graphics
                 Sprite(sunk,(i*CELL_SIZE+CELL_SIZE*(rowcols+1),j*CELL_SIZE))
                 
             if data["THEIRSUNK"] == shipNum: #ends the game with victory
-                Sprite((TextAsset("YOU WIN!!", fill=green,style= "bold 75pt Georgia")), (200, 50))
-                break
+                Sprite((TextAsset("YOU WIN!!", fill=green,style= "bold 75pt Georgia")), (220, 320))
+                data["gameOver"] = True #ends game
+
                 
             if data["SUNK"] == shipNum: #ends the game with loss
-                Sprite((TextAsset("YOU LOSE!!!!!!", fill=red,style= "bold 75pt Georgia")), (10, 50))
-                break
+                Sprite((TextAsset("YOU LOSE!!!!!!", fill=red,style= "bold 75pt Georgia")), (220, 320))
+                data["gameOver"] = True #ends game
+
+
                
 
             
 
 def mouseClick(event): #determines what happens when you click
 
-    
-    if data["totalClicks"] < shipNum: #deals with determining where our ships are
-        if (event.x//CELL_SIZE) <= (CELL_SIZE*rowcols) and event.y//CELL_SIZE <= CELL_SIZE*rowcols:
-            if data["board"][event.x//CELL_SIZE][event.y//CELL_SIZE] != "ship": #prevents placing multiple ships in a square
-                data["board"][event.x//CELL_SIZE][event.y//CELL_SIZE] = "ship" #sets a ship where we clicked
-                redrawAll()
-                data["totalClicks"] += 1
+    if data["gameOver"] == False:
+        if data["totalClicks"] < shipNum: #deals with determining where our ships are
+            if (event.x//CELL_SIZE) <= (CELL_SIZE*rowcols) and event.y//CELL_SIZE <= CELL_SIZE*rowcols:
+                if data["board"][event.x//CELL_SIZE][event.y//CELL_SIZE] != "ship": #prevents placing multiple ships in a square
+                    data["board"][event.x//CELL_SIZE][event.y//CELL_SIZE] = "ship" #sets a ship where we clicked
+                    redrawAll()
+                    data["totalClicks"] += 1
         
 
-    if data["totalClicks"] >= shipNum: #this part looks at our guesses of where their ships are
-        if data["compboard"] != "sunk" and data["compboard"] != "miss":#checks that we haven't guessed there before
-            if (event.x-(CELL_SIZE*(rowcols+1))//CELL_SIZE) >= CELL_SIZE*(rowcols+1) and (event.x-(CELL_SIZE*(rowcols+1))//CELL_SIZE) <= CELL_SIZE*((2*rowcols)+1): #makes sure your guess is in the right spot
-                if data["compboard"][(event.x-(CELL_SIZE*(rowcols+1)))//CELL_SIZE][event.y//CELL_SIZE] == "ship": #deals with what happens if it is a hit
-                    data["totalClicks"] += 1
-                    data["compboard"][(event.x-(CELL_SIZE*(rowcols+1)))//CELL_SIZE][event.y//CELL_SIZE] = "sunk" #sinks ship
-                    data["THEIRSUNK"] += 1 #adds to theirsunk count
-                else: #deals with what happens if it is a miss
-                    data["compboard"][(event.x-(CELL_SIZE*(rowcols+1)))//CELL_SIZE][event.y//CELL_SIZE] = "miss" #goes and sprites miss
-                    data["totalClicks"] += 1
-                redrawAll()
-                computerTurn()
-            
+        if data["totalClicks"] >= shipNum: #this part looks at our guesses of where their ships are
+            if data["compboard"] != "sunk" and data["compboard"] != "miss":#checks that we haven't guessed there before
+                if (event.x-(CELL_SIZE*(rowcols+1))//CELL_SIZE) >= CELL_SIZE*(rowcols+1) and (event.x-(CELL_SIZE*(rowcols+1))//CELL_SIZE) <= CELL_SIZE*((2*rowcols)+1): #makes sure your guess is in the right spot
+                    if data["compboard"][(event.x-(CELL_SIZE*(rowcols+1)))//CELL_SIZE][event.y//CELL_SIZE] == "ship": #deals with what happens if it is a hit
+                        data["totalClicks"] += 1
+                        data["compboard"][(event.x-(CELL_SIZE*(rowcols+1)))//CELL_SIZE][event.y//CELL_SIZE] = "sunk" #sinks ship
+                        data["THEIRSUNK"] += 1 #adds to theirsunk count
+                    else: #deals with what happens if it is a miss
+                        data["compboard"][(event.x-(CELL_SIZE*(rowcols+1)))//CELL_SIZE][event.y//CELL_SIZE] = "miss" #goes and sprites miss
+                        data["totalClicks"] += 1
+                    redrawAll()
+                    computerTurn()
+        return False    
 
 
 
 def pickComputerShips(): #sprites computer ships
     i = 0
     while i < shipNum: #makes sure right number of ships are created
-        rand1 = randint(0,rowcols-1)#randomly places them
+        rand1 = randint(0,rowcols-1) #randomly places them
         rand2 = randint(0,rowcols-1)
         if data["compboard"][rand1][rand2] != "ship": #makes sure it hasn't been guesed before
             data["compboard"][rand1][rand2] = "ship" #sets the ship there
@@ -93,22 +96,23 @@ def pickComputerShips(): #sprites computer ships
 
 
 def computerTurn(): #guesses where our ships are.
-    cord1 = randint(0,rowcols-1) #random guesses
-    cord2 = randint(0,rowcols-1)
+    if data["gameOver"] == False:
+        cord1 = randint(0,rowcols-1) #random guesses
+        cord2 = randint(0,rowcols-1)
     
-    if data["totalClicks"] > shipNum:
-        if data["board"][cord1][cord2] == "miss" or data["board"][cord1][cord2] == "sunk": #if it has been guessed before, it generates new numbers
-            computerTurn()
-        else:    
-            if data["board"][cord1][cord2] == "ship": #verifies if there is a ship there
-                data["board"][cord1][cord2] = "sunk" #sets the ship as sunk
-                data["SUNK"] += 1 #adds to sunk count
+        if data["totalClicks"] > shipNum:
+            if data["board"][cord1][cord2] == "miss" or data["board"][cord1][cord2] == "sunk": #if it has been guessed before, it generates new numbers
+                computerTurn()
+            else:    
+                if data["board"][cord1][cord2] == "ship": #verifies if there is a ship there
+                    data["board"][cord1][cord2] = "sunk" #sets the ship as sunk
+                    data["SUNK"] += 1 #adds to sunk count
             
-            else:
-                data["board"][cord1][cord2] = "miss" #lets the computer know that it has less a less effective intelligence agency than Lesotho, and can't find targets
-    redrawAll()        
+                else:
+                    data["board"][cord1][cord2] = "miss" #lets the computer know that it has less a less effective intelligence agency than Lesotho, and can't find targets
+        redrawAll()        
             
-    
+        return False
     
 
 if __name__== "__main__":
@@ -119,6 +123,7 @@ if __name__== "__main__":
     data["totalClicks"] = 0 #counts total mouse clicks
     data["SUNK"] = 0 #counts how many ships we have lost
     data["THEIRSUNK"] = 0 #counts how many ships they have lost
+    data["gameOver"] = False #sees if game is over
 
     
    
